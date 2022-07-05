2faa187043017a2a748fab72ad7c4993d67f9fe5
BubuNebed <nebedummiracle@gmail.com>
2022-07-05 03:59:18 -0700

    12th commit

---
+++ 0x02-functions_nested_loops/0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+
+	for (low = 'a'; low <= 'z'; low++)
+
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+
+	        printf("\n");
+
+		return (0);
+}
